555d3a51e0969f5c57786452f802e81865282657
Horilla <support@horilla.com>
2026-02-20 22:50:40 +0530

    [UPDT] GENERICS: DynamicForm is created inside get_form_class() and not importable

--- horilla_generics/methods.py
+++ horilla_generics/methods.py
@@ -1,1 +1,22 @@
 # Define your horilla_generics helper methods here
+
+from horilla_core.mixins import OwnerQuerysetMixin
+from horilla_generics.forms import HorillaModelForm
+
+
+def get_dynamic_form_for_model(model):
+    _model = model  # capture explicitly before class definition
+
+    class ResolvedDynamicForm(OwnerQuerysetMixin, HorillaModelForm):
+        class Meta:
+            model = _model  # use the captured local variable
+            fields = "__all__"
+            exclude = [
+                "created_at",
+                "updated_at",
+                "created_by",
+                "updated_by",
+                "additional_info",
+            ]
+
+    return ResolvedDynamicForm
