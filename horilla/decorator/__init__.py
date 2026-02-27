3e41a914420f7d9e28dbe9729c146686d9d00312
Horilla <support@horilla.com>
2026-02-28 00:42:47 +0530

    [ADD] HORILLA: Introduce horilla/decorator (__init__.py, wrapper.py) as the new decorator module, replacing horilla_core.decorato.

---
+++ horilla/decorator/__init__.py
@@ -0,0 +1,10 @@
+"""
+Horilla Decorator utilities.
+"""
+
+from horilla.decorator.wrapper import (
+    permission_required_or_denied,
+    permission_required,
+    htmx_required,
+    db_initialization,
+)
